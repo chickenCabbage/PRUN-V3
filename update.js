58e9c0434019c03984eb3716b4373e41364ebd34
chickenCabbage <alon.shiboleth@gmail.com>
2017-05-28 13:57:44 +0300

    Added Hivewroks time
    Cheers

--- update.js
+++ update.js
@@ -8,26 +8,28 @@ var title = fs.readFileSync("./update.txt").toString().split(",")[1]; //stores t
 client.on("fetch", function(){ //when client.fetch() is called
 	title = fs.readFileSync("./update.txt").toString().split(",")[1];
 	if(client.title != title) { //if the title changed - new page!
-		var date = new Date();
-		var time = date.getUTCHours() + ":" + date.getUTCMinutes() + " " +
-		date.getUTCDate() + "/" + (date.getUTCMonth() + 1) + "/" + date.getUTCFullYear() //date, in UTC
-
-		console.log(color.bold("UPDATED!") + " on " + time + ": " + client.title); //woo
+		var time = client.parsedDocument(".cc-publishtime").html() //the div content
+		.split("<br>")[0].split("posted ")[1] + " EST"; //remove excess HTML/data
 		fs.writeFile("./update.txt", time + "," + client.title + "," + client.images[0]); //change update.txt
+
+		console.log("\n" + color.bold("UPDATED!") + " on " + time + ": " + client.title); //woo
+		var date = new Date();
+		time = date.getUTCHours() + ":" + date.getUTCMinutes() + " " +
+		date.getUTCDate() + "/" + (date.getUTCMonth() + 1) + "/" + date.getUTCFullYear() //date, in UTC
+		console.log("Recoginzed on " + time + "\n");
 
 		title = client.title;
 
 		//ACTIVATE UPDATE MAILING HERE
 	}
-	else {
-		console.log("DEBUG NOPE");
-	}
+	else {/*CRY*/}
 });
 
 client.on("error", function(err) { //if an error occures
   console.log(err);
 });
 
+console.log("Starting now.");
 client.fetch(); //initialization
 
 setInterval(function() { //do this every 30 seconds (30,000 milis)
